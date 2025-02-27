74d9230a22ae0da4cb6e3504f1633f76677bdcea
André_Lucas <andrelucassvt99@gmail.com>
2025-02-27 08:58:33 -0300

    Initial Commit

---
+++ PaintARTests/PaintARTests.swift
@@ -0,0 +1,17 @@
+//
+//  PaintARTests.swift
+//  PaintARTests
+//
+//  Created by André  Lucas on 27/02/25.
+//
+
+import Testing
+@testable import PaintAR
+
+struct PaintARTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
